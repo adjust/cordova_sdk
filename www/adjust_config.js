ec9c3a02e5fa62d333e9fb54c904ad0abd86c0a9
uerceg <ugljesa@adjust.com>
2015-07-27 17:48:02 +0200

    Changing version number to 4.0.2

--- www/adjust_config.js
+++ www/adjust_config.js
@@ -3,7 +3,7 @@ function AdjustConfig(appToken, environment) {
     this.appToken = appToken;
     this.environment = environment;
 
-    this.sdkPrefix = "cordova4.0.1";
+    this.sdkPrefix = "cordova4.0.2";
 
     this.logLevel = null;
     this.defaultTracker = null;
