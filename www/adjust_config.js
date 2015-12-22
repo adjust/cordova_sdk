e9dea6919fa4092e3dab4b6bb1d1c8af66f5f075
uerceg <ugljesa@adjust.com>
2015-12-22 13:55:51 +0100

    New version 4.1.4

--- www/adjust_config.js
+++ www/adjust_config.js
@@ -3,7 +3,7 @@ function AdjustConfig(appToken, environment) {
     this.appToken = appToken;
     this.environment = environment;
 
-    this.sdkPrefix = "cordova4.1.3";
+    this.sdkPrefix = "cordova4.1.4";
 
     this.logLevel = null;
     this.defaultTracker = null;
