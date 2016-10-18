43861922947e14f2118ae76463fbab5927d0f2af
Obaied <abdullah.obaied@gmail.com>
2016-10-18 13:54:22 +0200

    add cordova-plugin-device plugin from Git and seperate the test scripts to iOS & Android

--- scripts/cordova-sample-build-android.sh
+++ scripts/cordova-sample-build-android.sh
@@ -18,25 +18,19 @@ echo -e "${GREEN}>>> Running Android build script ${NC}"
 cd ${SDK_DIR}
 ext/Android/build.sh; \cp -v ext/Android/adjust*.jar src/Android/adjust-android.jar
 
-echo -e "${GREEN}>>> Running iOS build script ${NC}"
-cd ${SDK_DIR}
-ext/iOS/build.sh
-
-echo -e "${GREEN}>>> Re-installing cordova plugin ${NC}"
+echo -e "${GREEN}>>> Re-installing plugins ${NC}"
 cd ${SDK_DIR}/${SAMPLE_DIR}
 cordova plugin add ${SDK_DIR}
 cordova plugin add cordova-plugin-console
-cordova plugin add cordova-plugin-customurlscheme
+cordova plugin add cordova-plugin-customurlscheme --variable URL_SCHEME=adjustExample
 cordova plugin add cordova-plugin-dialogs
 cordova plugin add cordova-plugin-whitelist
-cordova plugin add cordova-plugin-device
+cordova plugin add https://github.com/apache/cordova-plugin-device.git
 cordova plugin add cordova-universal-links-plugin
 
 echo -e "${GREEN}>>> Running Cordova build Android ${NC}"
 cordova build android
 
-echo -e "${GREEN}>>> Running Cordova build iOS ${NC}"
-cordova build ios --device
-
-echo -e "${GREEN}>>> Build successful. Installing APK on device ${NC}"
+echo -e "${GREEN}>>> Build successful. APK generated ${NC}"
+#echo -e "${GREEN}>>> Build successful. Installing APK on device ${NC}"
 #cordova run android --device --nobuild
---
+++ scripts/cordova-sample-build-ios.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Exit if any errors occur
+#set -e
+
+# Get the current directory (/scripts/ directory)
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Traverse up to get to the root directory
+SDK_DIR="$(dirname "$SCRIPTS_DIR")"
+SAMPLE_DIR=sample
+SDK_NAME=com.adjust.sdk
+
+RED='\033[0;31m' # Red color
+GREEN='\033[0;32m' # Green color
+NC='\033[0m' # No Color
+
+echo -e "${GREEN}>>> Running iOS build script ${NC}"
+cd ${SDK_DIR}
+ext/iOS/build.sh
+
+echo -e "${GREEN}>>> Re-installing plugins ${NC}"
+cd ${SDK_DIR}/${SAMPLE_DIR}
+cordova plugin add ${SDK_DIR}
+cordova plugin add cordova-plugin-console
+cordova plugin add cordova-plugin-customurlscheme --variable URL_SCHEME=adjustExample
+cordova plugin add cordova-plugin-dialogs
+cordova plugin add cordova-plugin-whitelist
+cordova plugin add https://github.com/apache/cordova-plugin-device.git
+cordova plugin add cordova-universal-links-plugin
+
+echo -e "${GREEN}>>> Running Cordova build iOS ${NC}"
+cordova build ios --device
+
+echo -e "${GREEN}>>> Build successful. IPA generated ${NC}"
+#echo -e "${GREEN}>>> Build successful. Installing APK on device ${NC}"
+#cordova run android --device --nobuild
