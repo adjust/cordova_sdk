ac75cf76c288b3d773010f53a2b71f2fd78be76a
Obaied <abdullah.obaied@gmail.com>
2017-12-17 07:14:45 +0100

    Update android build script

--- ext/Android/build.sh
+++ ext/Android/build.sh
@@ -5,13 +5,45 @@
 #   - makeJar
 # - Copy the JAR file to the root dir
 
+# End script if one of the lines fails
+# set -e
 
-BUILD_DIR=./sdk/Adjust
-OUT_DIR=./adjust/build/outputs
+if [ $# -ne 1 ]; then
+    echo $0: "usage: ./build.sh [debug || release]"
+    exit 1
+fi
+
+BUILD_TYPE=$1
+
+# Get the current directory (ext/android/)
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Traverse up to get to the root directory
+ROOT_DIR="$(dirname "$ROOT_DIR")"
+ROOT_DIR="$(dirname "$ROOT_DIR")"
+BUILD_DIR=sdk/Adjust
+JAR_OUT_DIR=plugin/src/Android
+
+RED='\033[0;31m' # Red color
+GREEN='\033[0;32m' # Green color
+NC='\033[0m' # No Color
 
 # cd to the called directory to be able to run the script from anywhere
 cd $(dirname $0) 
 
 cd $BUILD_DIR
-./gradlew clean makeReleaseJar
-mv -v ${OUT_DIR}/*.jar ../../../../src/Android/adjust-android.jar
+
+if [ "$BUILD_TYPE" == "debug" ]; then
+    JAR_IN_DIR=ext/android/sdk/Adjust/adjust/build/intermediates/bundles/debug
+    echo -e "${GREEN}>>> Running Gradle tasks: makeDebugJar${NC}"
+    ./gradlew makeDebugJar
+
+elif [ "$BUILD_TYPE" == "release" ]; then
+    JAR_IN_DIR=ext/android/sdk/Adjust/adjust/build/intermediates/bundles/release
+    echo -e "${GREEN}>>> Running Gradle tasks: makeReleaseJar${NC}"
+    ./gradlew makeReleaseJar
+fi
+
+echo -e "${GREEN}>>> Moving the jar from ${JAR_IN_DIR} to ${JAR_OUT_DIR} ${NC}"
+cd ${ROOT_DIR}
+mv -v ${JAR_IN_DIR}/*.jar ${ROOT_DIR}/${JAR_OUT_DIR}/adjust-android.jar
