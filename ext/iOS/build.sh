c8b358d3a5575d444b636af16b1e41436ce3530d
Obaied <abdullah.obaied@gmail.com>
2017-12-20 11:59:01 +0100

    Update submodules

--- ext/iOS/build.sh
+++ ext/iOS/build.sh
@@ -4,30 +4,30 @@
 set -e
 
 # Get the current directory
-SDK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Traverse up to get to the root directory
-SDK_DIR="$(dirname "$SDK_DIR")"
-SDK_DIR="$(dirname "$SDK_DIR")"
+ROOT_DIR="$(dirname "$ROOT_DIR")"
+ROOT_DIR="$(dirname "$ROOT_DIR")"
 
-SRC_DIR=ext/iOS/sdk
-LIB_OUT_DIR=src/iOS
-SCRIPT_DIR=scripts
+EXT_DIR=ext/iOS/sdk
+LIB_OUT_DIR=plugin/src/iOS
+SCRIPTS_DIR=scripts
 
 RED='\033[0;31m' # Red color
 GREEN='\033[0;32m' # Green color
 NC='\033[0m' # No Color
 
-cd ${SDK_DIR}
+cd ${ROOT_DIR}
 echo -e "${GREEN}>>> Removing old framework ${NC}"
 rm -rfv ${LIB_OUT_DIR}/AdjustSdk.framework
 
 echo -e "${GREEN}>>> building new framework ${NC}"
-cd ${SDK_DIR}/${SRC_DIR}
+cd ${ROOT_DIR}/${EXT_DIR}
 xcodebuild -target AdjustStatic -configuration Release clean build
 
 echo -e "${GREEN}>>> Copy built framework to designated location ${NC}"
-cd ${SDK_DIR}
-\cp -Rv ${SRC_DIR}/Frameworks/Static/AdjustSdk.framework ${LIB_OUT_DIR}
+cd ${ROOT_DIR}
+\cp -Rv ${EXT_DIR}/Frameworks/Static/AdjustSdk.framework ${LIB_OUT_DIR}
 
 echo -e "${GREEN}>>> Running symlink fix ${NC}"
-${SCRIPT_DIR}/symlink_fix.sh
+${SCRIPTS_DIR}/symlink_fix.sh
