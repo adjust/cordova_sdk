cc57f2e7a0fa9e821bcfd0d09aa22b8bd030753c
Obaied <abdullah.obaied@gmail.com>
2018-01-10 11:39:13 +0100

    Remove com.adjust.sdk.purchase from example app

--- example/plugins/com.adjust.sdk.purchase/ext/Android/sdk/AdjustPurchase/adjust_purchase/src/main/java/com/adjust/sdk/purchase/OnADJPVerificationFinished.java
+++
@@ -1,8 +0,0 @@
-package com.adjust.sdk.purchase;
-
-/**
- * Created by uerceg on 04/12/15.
- */
-public interface OnADJPVerificationFinished {
-    void onVerificationFinished(ADJPVerificationInfo answer);
-}
