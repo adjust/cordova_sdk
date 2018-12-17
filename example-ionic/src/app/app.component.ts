7bf2a239b69a99009bb81e16f272302025268b58
serj <srdjan@adjust.com>
2018-12-17 13:47:00 +0100

    ionic example added

---
+++ example-ionic/src/app/app.component.ts
@@ -0,0 +1,82 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { Adjust, AdjustConfig, AdjustAttribution, AdjustLogLevel, AdjustEnvironment, AdjustEventFailure, AdjustEventSuccess, AdjustSessionSuccess, AdjustSessionFailure } from '@ionic-native/adjust';
+
+import { HomePage } from '../pages/home/home';
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any = HomePage;
+
+  constructor(platform: Platform, adjust: Adjust, statusBar: StatusBar, splashScreen: SplashScreen) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+
+      const config = new AdjustConfig('2fm9gkqubvpc', AdjustEnvironment.Sandbox);
+      config.logLevel = AdjustLogLevel.Verbose;
+      config.shouldLaunchDeeplink = true;
+      config.attributionCallback = (attribution: AdjustAttribution) => {
+        console.log("### Attribution callback received");
+
+        console.log("Tracker token = " + attribution.trackerToken);
+        console.log("Tracker name = " + attribution.trackerName);
+        console.log("Network = " + attribution.network);
+        console.log("Campaign = " + attribution.campaign);
+        console.log("Adgroup = " + attribution.adgroup);
+        console.log("Creative = " + attribution.creative);
+        console.log("Click label = " + attribution.clickLabel);
+        console.log("Adid = " + attribution.adid);
+      }
+      config.eventTrackingFailedCallback = (eventFailed: AdjustEventFailure) => {
+        console.log("### Event tracking failed callback received");
+
+        console.log("Message: " + eventFailed.message);
+        console.log("Timestamp: " + eventFailed.timestamp);
+        console.log("Adid: " + eventFailed.adid);
+        console.log("Event token: " + eventFailed.eventToken);
+        console.log("Will retry: " + eventFailed.willRetry);
+        console.log("JSON response: " + eventFailed.jsonResponse);
+      }
+      config.eventTrackingSucceededCallback = (eventSuccess: AdjustEventSuccess) => {
+        console.log("### Event tracking succeeded callback received");
+
+        console.log("Message: " + eventSuccess.message);
+        console.log("Timestamp: " + eventSuccess.timestamp);
+        console.log("Adid: " + eventSuccess.adid);
+        console.log("Event token: " + eventSuccess.eventToken);
+        console.log("JSON response: " + eventSuccess.jsonResponse);
+      }
+      config.sessionTrackingSucceededCallback = (sessionSuccess: AdjustSessionSuccess) => {
+        console.log("### Session tracking succeeded callback received");
+
+        console.log("Message: " + sessionSuccess.message);
+        console.log("Timestamp: " + sessionSuccess.timestamp);
+        console.log("Adid: " + sessionSuccess.adid);
+        console.log("JSON response: " + sessionSuccess.jsonResponse);
+      }
+      config.sessionTrackingFailedCallback = (sessionFailed: AdjustSessionFailure) => {
+        console.log("### Session tracking failed callback received");
+
+        console.log("Message: " + sessionFailed.message);
+        console.log("Timestamp: " + sessionFailed.timestamp);
+        console.log("Adid: " + sessionFailed.adid);
+        console.log("Will retry: " + sessionFailed.willRetry);
+        console.log("JSON response: " + sessionFailed.jsonResponse);
+      }
+      config.deferredDeeplinkCallback = (uri: string) => {
+        console.log("### Deferred Deeplink Callback received");
+
+        console.log("URL: " + uri);
+      }
+
+      adjust.create(config);
+    });
+  }
+}
+
---
+++ example-ionic/src/pages/home/home.ts
@@ -0,0 +1,102 @@
+import { Component } from '@angular/core';
+import { NavController, AlertController } from 'ionic-angular';
+import { Adjust, AdjustEvent } from '@ionic-native/adjust';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public alertCtrl: AlertController, public navCtrl: NavController, public adjust: Adjust) {
+
+  }
+
+  trackSimpleEvent() {
+    const adjustEvent: AdjustEvent = new AdjustEvent('g3mfiw');
+    this.adjust.trackEvent(adjustEvent);
+  }
+
+  trackRevenueEvent() {
+    const adjustEvent: AdjustEvent = new AdjustEvent('a4fd35');
+    adjustEvent.setRevenue(0.01, 'USD');
+    this.adjust.trackEvent(adjustEvent);
+  }
+
+  trackCallbackEvent() {
+    const adjustEvent: AdjustEvent = new AdjustEvent('34vgg9');
+    adjustEvent.addCallbackParameter('key', 'stuff');
+    adjustEvent.addCallbackParameter('x', 'y');
+    adjustEvent.addCallbackParameter('key', 'lock');
+    this.adjust.trackEvent(adjustEvent);
+  }
+
+  trackPartnerEvent() {
+    const adjustEvent: AdjustEvent = new AdjustEvent('w788qs');
+    adjustEvent.addPartnerParameter('foo', 'bar');
+    adjustEvent.addPartnerParameter('x', 'y');
+    adjustEvent.addPartnerParameter('foo', 'foot');
+    adjustEvent.addPartnerParameter('x', 'z');
+    this.adjust.trackEvent(adjustEvent);
+  }
+
+  enableOfflineMode() {
+    this.adjust.setOfflineMode(true);
+  }
+
+  disableOfflineMode() {
+    this.adjust.setOfflineMode(false);
+  }
+
+  enableSdk() {
+    this.adjust.setEnabled(true);
+  }
+
+  disableSdk() {
+    this.adjust.setEnabled(false);
+  }
+
+  isSdkEnabled() {
+    this.adjust.isEnabled().then((isEnabled) => {
+      let alert = this.alertCtrl.create({
+        title: 'Adjust - Is SDK Enabled?',
+        subTitle: 'SDK enabled = ' + isEnabled,
+        buttons: ['OK']
+      });
+      alert.present();
+    });
+  }
+
+  getIds() {
+    this.adjust.getIdfa().then((idfa) => {
+      console.log("IDFA = " + idfa);
+    });
+
+    this.adjust.getGoogleAdId().then((gpsAdId) => {
+        console.log("Google Ad Id = " + gpsAdId);
+    });
+
+    this.adjust.getAmazonAdId().then((gpsAdId) => {
+        console.log("Amazon Ad Id = " + gpsAdId);
+    });
+
+    this.adjust.getAdid().then((adid) => {
+        console.log("Adid = " + adid);
+    });
+  }
+
+  gdprForgetMe() {
+    this.adjust.gdprForgetMe();
+  }
+
+  getAttribution() {
+    this.adjust.getAttribution().then((att) => {
+      let alert = this.alertCtrl.create({
+        title: 'Adjust Attribution',
+        subTitle: JSON.stringify(att),
+        buttons: ['OK']
+      });
+      alert.present();
+    });
+  }
+}
