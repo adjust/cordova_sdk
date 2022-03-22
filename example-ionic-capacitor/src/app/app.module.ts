3d495501c2ede36eab0649edff905f031f8d93c1
Aditi3 <Aditi3@users.noreply.github.com>
2022-03-22 10:38:06 +0100

    chore: update to cordova example project

--- example-ionic-capacitor/src/app/app.module.ts
+++ example-ionic-capacitor/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { Adjust, AdjustConfig, AdjustEnvironment, AdjustLogLevel } from '@awesome-cordova-plugins/adjust/ngx';
+import { Adjust, AdjustConfig, AdjustEnvironment, AdjustLogLevel } from 'com.adjust.sdk/ionic-native';
 
 @NgModule({
   declarations: [AppComponent],
