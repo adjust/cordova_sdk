aaa1f87b0d593dd89fbc88174cfadccbd4c227d5
uerceg <ugi@adjust.com>
2024-01-24 12:59:53 +0100

    feat: update version number to 4.37.0

--- www/adjust.js
+++ www/adjust.js
@@ -171,7 +171,7 @@ var Adjust = {
     },
 
     getSdkPrefix: function () {
-        return 'cordova4.35.1';
+        return 'cordova4.37.0';
     },
 
     addSessionCallbackParameter: function(key, value) {
