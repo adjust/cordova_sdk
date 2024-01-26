1dd86c7157466ad7ecff5f9d1555b1e2091234ac
uerceg <ugi@adjust.com>
2024-01-26 11:27:38 +0100

    chore: rebuild example ionic capacitor app with latest tools' version

---
+++ example-ionic-capacitor/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'example-ionic-capacitor',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
--- example-ionic-capacitor/src/app/app.module.ts
+++ example-ionic-capacitor/src/app/app.module.ts
@@ -4,16 +4,16 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 import { Adjust, AdjustConfig, AdjustEnvironment, AdjustLogLevel } from '@awesome-cordova-plugins/adjust/ngx';
 
 @NgModule({
   declarations: [AppComponent],
-  entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [Adjust, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [Adjust,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
