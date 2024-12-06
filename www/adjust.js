c041fef284dbfb4839b53bfe60ef2ccb24698bc9
uerceg <ugi@adjust.com>
2024-12-06 09:32:51 +0100

    feat: update version number 5.0.3

--- www/adjust.js
+++ www/adjust.js
@@ -109,7 +109,7 @@ var Adjust = {
     },
 
     getSdkPrefix: function () {
-        return 'cordova5.0.2';
+        return 'cordova5.0.3';
     },
 
     addGlobalCallbackParameter: function(key, value) {
