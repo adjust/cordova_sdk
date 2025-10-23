28fc8881c6c683ec21fa987833ca3802c0e6d9d0
uerceg <ugi@adjust.com>
2025-10-23 16:41:49 +0200

    test: run tests

--- test/app/www/js/index.js
+++ test/app/www/js/index.js
@@ -21,7 +21,7 @@ var app = {
             }
 
             var urlOverwrite = "";
-            var ipAddress = "192.168.86.217";
+            var ipAddress = "192.168.0.30";
             if (device.platform === "Android") {
                 urlOverwrite = "https://" + ipAddress + ":8443";
             } else if (device.platform === "iOS") {
